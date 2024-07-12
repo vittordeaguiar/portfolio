3a68c777f44b67b9df885aed376e3abdcf207cc9
Vittor de Aguiar <vittoraguiar93@gmail.com>
2024-07-11 21:23:02 -0300

    adicionando HTTPS e favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,20 @@
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Vittor de Aguiar',
+      htmlAttrs: {
+        lang: 'pt-BR',
+      },
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/icons/favicon.png',
+        },
+      ],
+    },
+  },
   devtools: { enabled: true },
-  css: [
-    "~/assets/css/main.css",
-    "tippy.js/themes/light-border.css",
-    "tippy.js/dist/tippy.css",
-  ],
-  modules: ["@nuxtjs/tailwindcss"],
+  css: ['~/assets/css/main.css', 'tippy.js/themes/light-border.css', 'tippy.js/dist/tippy.css'],
+  modules: ['@nuxtjs/tailwindcss'],
 });
