b49305957477e5f5be262dde54e82f9fe7f67243
vittordeaguiar <vittoraguiar93@gmail.com>
2025-11-28 09:47:47 -0300

    add vercel analytics

--- app/app.vue
+++ app/app.vue
@@ -17,10 +17,12 @@
   </div>
 
   <SpeedInsights />
+  <Analytics />
 </template>
 
 <script setup lang="ts">
 import { SpeedInsights } from "@vercel/speed-insights/nuxt";
+import { Analytics } from '@vercel/analytics/nuxt'
 const { initTheme } = useTheme();
 const { setupScrollObserver } = useScrollAnimation();
 
